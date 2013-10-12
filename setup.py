16aba51cf2e1b9586f6dfb2bee918046ef68ed6f
Nils Philippsen <nils@tiptoe.de>
2013-10-12 13:01:22 +0200

    upgrade to ToscaWidgets2, fancybox 1.3.4, mousewheel 3.0.4

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="tw.jquery.fancybox",
+    name="tw2.jquery.fancybox",
     version="0.0",
-    description="jQuery FancyBox integration for ToscaWidgets",
+    description="jQuery FancyBox integration for ToscaWidgets 2",
     author="Nils Philippsen",
     author_email="nils@tiptoe.de",
     #url=
     #download_url=
-    install_requires=["ToscaWidgets>=0.9.8", "tw.jquery>=0.9.5"],
+    install_requires=["tw2.core>=2.0", "tw2.jquery>=2.0"],
     packages=find_packages(),
-    namespace_packages = ['tw.jquery'],
+    namespace_packages = ['tw2', 'tw2.jquery'],
     zip_safe=False,
     include_package_data=True,
     #test_suite="nose.collector"
-    package_data={"tw.jquery.fancybox": ["static/*"]},
+    package_data={"tw2.jquery.fancybox": ["static/*"]},
     entry_points="""
-        [toscawidgets.widgets]
-        widgets = tw.jquery.fancybox
+        [tw2.widgets]
+        widgets = tw2.jquery.fancybox
     """,
-    keywords = ["toscawidgets.widgets"],
+    keywords = ["tw2.widgets"],
     classifiers = [
         "Development Status :: 3 - Alpha",
         "Environment :: Web Environment",
---
+++ tw2/jquery/fancybox/__init__.py
@@ -0,0 +1,199 @@
+# -*- coding: utf-8 -*-
+#
+# tw2.jquery.fancybox - a jQuery FancyBox widget
+#
+# Copyright © 2010, 2013 Nils Philippsen <nils@tiptoe.de>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import pkg_resources
+pkg_resources.require("tw2.jquery >= 2.0")
+
+from tw2.core import Widget, JSLink, CSSLink, Link
+import tw2.core as twc
+from tw2.jquery.base import jquery_js, jQuery
+
+__all__ = ("mousewheel_js", "fancybox_js", "fancybox_css", "FancyBox")
+
+FANCYBOX_VERSION = "1.3.4"
+MOUSEWHEEL_VERSION = "3.0.4"
+
+# JS Links
+
+mousewheel_js = JSLink(modname=__name__,
+        filename="static/jquery.mousewheel-%s.pack.js" % MOUSEWHEEL_VERSION)
+
+fancybox_js = JSLink(modname=__name__,
+        filename="static/jquery.fancybox-%s.pack.js" % FANCYBOX_VERSION)
+
+# CSS Link
+
+fancybox_css = CSSLink(modname=__name__,
+        filename="static/jquery.fancybox-%s.css" % FANCYBOX_VERSION)
+
+# Other resources
+
+static_resources = Link(modname=__name__, filename="static", whole_dir=True)
+
+# Widget
+
+class FancyBox(Widget):
+    resources = [fancybox_css, jquery_js, mousewheel_js, fancybox_js,
+            static_resources]
+
+    fancybox_attrnames = (
+            'padding', 'margin', 'opacity', 'modal', 'cyclic', 'scrolling',
+            'width', 'height', 'autoScale', 'autoDimensions', 'centerOnScroll',
+            'ajax', 'swf', 'hideOnOverlayClick', 'hideOnContentClick',
+            'overlayShow', 'overlayOpacity', 'overlayColor',
+            'titleShow', 'titlePosition', 'titleFormat',
+            'transitionIn', 'transitionOut', 'speedIn', 'speedOut',
+            'changeSpeed', 'changeFade', 'easingIn', 'easingOut',
+            'showCloseButton', 'showNavArrows', 'enableEscapeButton',
+            'onStart', 'onCancel', 'onComplete', 'onCleanup', 'onClosed')
+
+    selector = twc.Param("Selector specifying images to be decorated")
+
+    padding = twc.Param("Space between FancyBox wrapper and content")
+
+    margin = twc.Param("Space between viewport and FancyBox wrapper")
+
+    opacity = twc.Param("When True, transparency of content is changed "
+                        "for elastic transitions")
+
+    modal = twc.Param("When True, 'overlayShow' is set to True and "
+                      "'hideOnOverlayClick', 'hideOnContentClick', "
+                      "'enableEscapeButton', 'showCloseButton' "
+                      "are set to False")
+
+    cyclic = twc.Param("When True, galleries will be cyclic, allowing you to "
+                       "keep pressing next/back.")
+
+    scrolling = twc.Param("Set the overflow CSS property to create or hide "
+                          "scrollbars. Can be set to 'auto', 'yes', or 'no'")
+
+    width = twc.Param("Width for content types 'iframe' and 'swf'. Also set "
+                      "for inline content if 'autoDimensions' is set to False")
+
+    height = twc.Param("Height for content types 'iframe' and 'swf'. Also set "
+                       "for inline content if 'autoDimensions' is set to False")
+
+    autoScale = twc.Param("If True, FancyBox is scaled to fit in viewport")
+
+    autoDimensions = twc.Param("For inline and ajax views, resizes the view "
+                               "to the element received. Make sure it has "
+                               "dimensions otherwise this will give "
+                               "unexpected results")
+
+    centerOnScroll = twc.Param("When True, FancyBox is centered while "
+                               "scrolling page")
+
+    ajax = twc.Param("Ajax options. Note: 'error' and 'success' will be "
+                     "overwritten by FancyBox")
+
+    swf = twc.Param("Params to put on the swf object")
+
+    hideOnOverlayClick = twc.Param("Toggle if clicking the overlay should "
+                                   "close FancyBox")
+
+    hideOnContentClick = twc.Param("Toggle if clicking the content should "
+                                   "close FancyBox")
+
+    overlayShow = twc.Param("Toggle overlay")
+
+    overlayOpacity = twc.Param("Opacity of the overlay (from 0 to 1; "
+                               "default - 0.3)")
+
+    overlayColor = twc.Param("Color of the overlay")
+
+    titleShow = twc.Param("Toggle title")
+
+    titlePosition = twc.Param("The position of title. Can be set to "
+                              "'outside', 'inside' or 'over'")
+
+    titleFormat = twc.Param("Callback to customize title area. You can set "
+                            "any html - custom image counter or even custom "
+                            "navigation")
+
+    transitionIn = transitionOut = twc.Param(
+            "The transition type. Can be set to 'elastic', 'fade' or 'none'")
+
+    speedIn = speedOut = twc.Param(
+            "Speed of the fade and elastic transitions, in milliseconds")
+
+    changeSpeed = twc.Param("Speed of resizing when changing gallery items, "
+                            "in milliseconds")
+
+    changeFade = twc.Param("Speed of the content fading while changing "
+                           "gallery items")
+
+    easingIn = easingOut = twc.Param("Easing used for elastic animations")
+
+    showCloseButton = twc.Param("Toggle close button")
+
+    showNavArrows = twc.Param("Toggle navigation arrows")
+
+    enableEscapeButton = twc.Param("Toggle if pressing Esc button closes "
+                                   "FancyBox")
+
+    onStart = twc.Param("Will be called right before attempting to load "
+                        "the content")
+
+    onCancel = twc.Param("Will be called after loading is canceled")
+
+    onComplete = twc.Param("Will be called once the content is displayed")
+
+    onCleanup = twc.Param("Will be called just before closing")
+
+    onClosed = twc.Param("Will be called once FancyBox is closed")
+
+    def prepare(self):
+        super(FancyBox, self).prepare()
+        if not self.selector:
+            raise ValueError("FancyBox needs a selector")
+
+        if (hasattr(self, 'scrolling') and
+            self.scrolling not in ('auto', 'yes', 'no')):
+            raise ValueError("Scrolling must be 'auto', 'yes' or 'no'")
+
+        if (hasattr(self, 'overlayOpacity') and
+            not (0.0 < float(self.overlayOpacity) < 1.0)):
+            raise ValueError("Overlay opacity must be between 0.0 and 1.0")
+
+        if (hasattr(self, 'titlePosition') and
+            self.titlePosition not in ('outside', 'inside', 'over')):
+            raise ValueError("Title position must be 'outside', 'inside' "
+                             "or 'over'")
+
+        if (hasattr(self, 'transitionIn') and
+            self.transitionIn not in ('elastic', 'fade', 'none')):
+            raise ValueError("In transition must be 'elastic', 'fade' or "
+                             "'none'")
+
+        if (hasattr(self, "transitionOut") and
+            self.transitionOut not in ('elastic', 'fade', 'none')):
+            raise ValueError("Out transition must be 'elastic', 'fade' or "
+                             "'none'")
+
+        params = {}
+        for name in self.fancybox_attrnames:
+            if hasattr(self, name):
+                params[name] = getattr(self, name)
+
+        call = jQuery(self.selector).fancybox(params)
+        self.add_call(call)
+
+    def generate_output(self, displays_on):
+        return u""
